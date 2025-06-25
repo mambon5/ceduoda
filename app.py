9e395e72d4178a6e42cc8ad25b919cf2795850f7
roma <monantoniet@gmail.com>
2025-06-25 15:43:53 +0200

    primera versió de la web de ceduoda

---
+++ app.py
@@ -0,0 +1,19 @@
+# Estructura del projecte:
+# ├── app.py
+# ├── templates/
+# │   └── index.html
+# └── static/
+#     ├── css/
+#     │   └── style.css
+#     └── images/
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
