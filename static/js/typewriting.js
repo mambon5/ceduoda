49414a31fadc168ca8d97237571b5588536aac93
roma <monantoniet@gmail.com>
2025-06-26 19:57:07 +0200

    remodelat de la web

---
+++ static/js/typewriting.js
@@ -0,0 +1,22 @@
+const text = "Classes de tot!";
+const typewriter = document.getElementById("typewriter");
+const cursor = document.querySelector(".cursor");
+
+let i = 0;
+function typeEffect() {
+    if (i < text.length) {
+        typewriter.innerHTML += text.charAt(i);
+        i++;
+        setTimeout(typeEffect, 120);
+    }
+}
+
+window.onload = () => {
+    typeEffect();
+
+    // 🔹 Atura el cursor després de 10 segons
+    setTimeout(() => {
+        cursor.style.animation = "none";
+        cursor.style.opacity = 0;
+    }, 3000+20); // 5 segs approx
+};
