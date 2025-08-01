56aa1f2a17771980e7d1805b2b49aedae7d31025
Romà <monantoniet@gmail.com>
2025-08-02 00:03:05 +0200

    menu lateral s'amaga en mobil quan cliquem a un altre lloc, en pc sempre present

--- static/js/selecc_nav_segons_actiu.js
+++ static/js/selecc_nav_segons_actiu.js
@@ -30,3 +30,32 @@ function toggleMenu() {
   const menu = document.querySelector('.side-menu');
   menu.classList.toggle('visible');
 }
+
+
+// amagar el side menu quan es cliqui a un altre lloc de la pantalla:
+const menu = document.querySelector('.side-menu');
+  const toggleBtn = document.querySelector('.menu-toggle');
+
+  function toggleMenu() {
+    if (menu.classList.contains('visible') &&  window.innerWidth <= 768) {
+      menu.classList.remove('visible');
+      menu.classList.add('hidden');
+    } else {
+      menu.classList.add('visible');
+      menu.classList.remove('hidden');
+    }
+  }
+
+  // Al carregar la pàgina, assegura que el menú està amagat
+  if (window.innerWidth <= 768)   menu.classList.add('hidden');
+
+  document.addEventListener('click', function(event) {
+    const isClickInsideMenu = menu.contains(event.target);
+    const isClickOnToggle = toggleBtn.contains(event.target);
+
+    // Si el clic NO és dins menú ni toggle i el menú és visible, l'amaguem
+    if (!isClickInsideMenu && !isClickOnToggle && menu.classList.contains('visible')) {
+      menu.classList.remove('visible');
+      menu.classList.add('hidden');
+    }
+  });
